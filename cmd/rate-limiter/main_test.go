55c0bbad7a07c36c093bbc34ead7b3f0d6859d61
gilbertom <gilberto.maki@gmail.com>
2024-06-30 15:16:31 -0300

    Add Test TestRateLimiterExcedendoLimiteUsandoToken

--- cmd/rate-limiter/main_test.go
+++ cmd/rate-limiter/main_test.go
@@ -146,43 +146,83 @@ loopMain:
 	}
 }
 
+func TestRateLimiterExcedendoLimiteUsandoToken(t *testing.T) {
+	err := godotenv.Load("/cmd/rate-limiter/.env")
+	// err := godotenv.Load()
+	if err != nil {
+			log.Fatalf("Error trying to load env variables: %v", err)
+	}
+	
+	os.Setenv("ON_REQUESTS_EXCEEDED_BLOCK_BY", "TOKEN")
+	env := config.LoadEnv()
+	
+	rateLimit := env.MaxRequestsAllowedByToken + 1
+	serverURL := "http://localhost:8080/"
+	apiKey := "123456"
 
-// func TestRateLimiterExcedendoLimitePorIp(t *testing.T) {
-// 	err := godotenv.Load("/cmd/rate-limiter/.env")
-// 	if err != nil {
-// 			log.Fatalf("Error trying to load env variables: %v", err)
-// 	}
-// 	env := config.LoadEnv()
+	go func() {
+		main()
+	}()
+
+	time.Sleep(time.Second)
+
+	client := &http.Client{}
+	var resp *http.Response
+
+	for i := 1; i <= rateLimit; i++ {
+		req, err := http.NewRequest("GET", serverURL, nil)
+		if err != nil {
+			t.Fatalf("Failed to create request: %v", err)
+		}
+		req.Header.Set("API_KEY", apiKey)
 		
-// 	rateLimit := env.MaxRequestsAllowedByIP + 1
-// 	serverURL := "http://localhost:8080/"
+		resp, err = client.Do(req)
+		if err != nil {
+			t.Fatalf("Failed to make request: %v", err)
+			break
+		}
+		defer resp.Body.Close()
+	}
+	
+	if resp.StatusCode != http.StatusTooManyRequests {
+		t.Errorf("Expected status 429 but got %v on request %d", resp.StatusCode, 1)
+	}
+}
 
-// 	go func() {
-// 		main()
-// 	}()
+func TestRateLimiterExcedendoLimitePorIpAguardaLiberacaoBloqueio(t *testing.T) {
+	err := godotenv.Load("/cmd/rate-limiter/.env")
+	if err != nil {
+			log.Fatalf("Error trying to load env variables: %v", err)
+	}
+	env := config.LoadEnv()
+		
+	rateLimit := env.MaxRequestsAllowedByIP + 1
+	serverURL := "http://localhost:8080/"
 
-// 	time.Sleep(time.Second)
+	go func() {
+		main()
+	}()
 
-// 	client := &http.Client{}
+	time.Sleep(time.Second)
 
-// 	var resp *http.Response
+	client := &http.Client{}
 
-// 	for i := 1; i <= rateLimit; i++ {
-// 		resp, err = client.Get(serverURL)
-// 		if err != nil {
-// 			t.Fatalf("Failed to make request: %v", err)
-// 			break
-// 		}
+	var resp *http.Response
 
-// 		// if resp.StatusCode == http.StatusTooManyRequests {
-// 		// 	// t.Errorf("Expected status 429 but got %v on request %d", resp.StatusCode, i+1)
-// 		// 	break
-// 		// }
-// 		resp.Body.Close()
-// 	}
+	for i := 1; i <= rateLimit; i++ {
+		resp, err = client.Get(serverURL)
+		if err != nil {
+			t.Fatalf("Failed to make request: %v", err)
+			break
+		}
 
-// 	if resp.StatusCode != http.StatusTooManyRequests {
-// 		t.Errorf("Expected status 429 but got %v on request %d", resp.StatusCode, rateLimit)
-// 	}
-// }
+		resp.Body.Close()
+	}
 
+	if resp.StatusCode != http.StatusTooManyRequests {
+		t.Errorf("Expected status 429 but got %v on request %d", resp.StatusCode, rateLimit)
+	}
+
+	log.Printf("Aguardando %d segundos até ocorrer o desbloqueio por IP", env.TimeToReleaseRequestsIP)
+	time.Sleep(time.Duration(env.TimeToReleaseRequestsIP))
+}
